96ec09133cce5ca6b38e7603c7eeef8091c5d29c
thenefenyak <nefenyak92@yandex.ru>
2024-08-29 11:24:16 +0300

    add: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function toReadable (number) {
-  
+  const numbers = [ 
+    'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',
+  ];
+  const tens = [
+    'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',
+  ];
+
+  if (number === 0) return 'zero';
+
+  let result = '';
+
+  if (Math.floor(number / 100) > 0) {
+    result += numbers[Math.floor(number / 100)] + ' hundred';
+  }
+ 
+  let leftover = number % 100;
+
+  if (leftover > 0 && leftover < 20) {
+    result += numbers[leftover];
+  } else {
+    result += tens[Math.floor(leftover / 10)];
+    if (leftover % 10 > 0) {
+      result += '' + numbers[leftover % 10];
+    }
+  }
+
+  return result.trim();
+
 }
+
+
