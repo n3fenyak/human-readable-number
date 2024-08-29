371f67215868412f8303902c4a4a8f43d375dd7f
thenefenyak <nefenyak92@yandex.ru>
2024-08-29 11:31:01 +0300

    update: index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-module.exports = function toReadable (number) {
-  const numbers = [ 
-    'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
-    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',
+module.exports = function toReadable(number) {
+  const numbers = [
+      '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 
+      'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 
+      'seventeen', 'eighteen', 'nineteen'
   ];
+  
   const tens = [
-    'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',
+      '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
   ];
 
   if (number === 0) return 'zero';
@@ -12,22 +14,21 @@ module.exports = function toReadable (number) {
   let result = '';
 
   if (Math.floor(number / 100) > 0) {
-    result += numbers[Math.floor(number / 100)] + ' hundred';
+      result += numbers[Math.floor(number / 100)] + ' hundred ';
   }
- 
+
   let leftover = number % 100;
 
   if (leftover > 0 && leftover < 20) {
-    result += numbers[leftover];
+      result += numbers[leftover];
   } else {
-    result += tens[Math.floor(leftover / 10)];
-    if (leftover % 10 > 0) {
-      result += '' + numbers[leftover % 10];
-    }
+      result += tens[Math.floor(leftover / 10)];
+      if (leftover % 10 > 0) {
+          result += ' ' + numbers[leftover % 10];
+      }
   }
 
   return result.trim();
-
 }
 
 
